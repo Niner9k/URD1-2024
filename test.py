a78d0c5de88d18a69718ac35cef8e433804098fc
Thanakit Thanasuwanditee <tthanas@prism-4.apex.cmkl.ac.th>
2024-11-06 20:40:09 +0700

    test-cases

--- test.py
+++ test.py
@@ -2,5 +2,7 @@ import torch
 from ultralytics import YOLO
 # model = YOLO("/home/tthanas/URD1-2024/runs/detect/train/weights/best.pt")
 model = YOLO("runs/detect/train5/weights/best.pt")
-# result = model(source='/home/tthanas/URD1-2024/Pics/blackmen.jpg', show=True, conf=0.4, save =True)
-result = model(source='Pics/kid1.jpg', show=True, conf=0.4, save =True)
+# result = model(source='Pics/blackmen.jpg', show=True, conf=0.4, save =True)
+# result = model(source='Pics/kid1.jpg', show=True, conf=0.4, save =True)
+# result = model(source='Pics/kid2.jpeg', show=True, conf=0.4, save =True)
+result = model(source='Pics/oreo.png', show=True, conf=0.4, save =True)

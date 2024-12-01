6fd6745fea9874a6fae9db77e192121035d3b535
Thanakit Thanasuwanditee <tthanas@archon-2.apex.cmkl.ac.th>
2024-12-01 17:57:02 +0700

    Your temporary commit message

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 import torch
 from ultralytics import YOLO
 # model = YOLO("/home/tthanas/URD1-2024/runs/detect/train/weights/best.pt")
-model = YOLO("runs/detect/train5/weights/best.pt")
+# model = YOLO("runs/detect/train5/weights/best.pt")
+model = YOLO("runs/detect/train7/weights/best.pt")
 # modelD = YOLO("runs/detect/train6/weights/best.pt")
 # modelS = YOLO("runs/detect/train7/weights/best.pt")
 # result = model(source='Pics/blackmen.jpg', show=True, conf=0.4, save =True)
 # result = model(source='Ignore/Pics/kid1.jpg', show=True, conf=0.4, save =True)
 # result = model(source='Pics/kid2.jpeg', show=True, conf=0.4, save =True)
-result = model(source='Ignore/Pics/kid-and-adult.jpg', show=True, conf=0.4, save =True)
+# result = model(source='Ignore/Pics/kid-and-adult.jpg', show=True, conf=0.4, save =True)
 # result = model(source='Pics/satwik.jpg', show=True, conf=0.4, save =True)
 # result = model(source='Video/test1.mov', show=True, conf=0.4, save =True)
 # result = modelS(source = "Ignore/Pics/9.jpg", show = True, conf=0.4, save =True)
+result = model(source = "Ignore/Pics/sleepy.jpg", show = True, conf=0.4, save =True)
